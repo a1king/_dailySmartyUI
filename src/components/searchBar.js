e4e11dca201acfa3efef84883e92e164d6001bd0
Fernando M <626melo@gmail.com>
2021-02-20 20:36:15 -0600

    "setup app routes"

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import { field, reduxForm } from "redux-form";
 
 class SearchBar extends Component {
-  render() {
+  handleFormSubmit = function ({ query }) {
+    console.log("trying to handle subimit for query", query);
+  };
+
+  renderInput(field) {
     return (
-      <form>
-        <input placeholder="Search DailySmarty" />
+      <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    );
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form
+        className="search-bar"
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+      >
+        <Field name="query" component={this.renderInput} />
       </form>
     );
   }
 }
 
-export default SearchBar;
+SearchBar = reduxForm({
+  form: "searchBar",
+})(SearchBar);
